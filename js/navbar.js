d9dd5b7415653263d0771e6d01a222bd41bca86c
FadumaM <faduma.mohammed@gmail.com>
2016-10-30 03:50:00 +0000

    created landing page

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+  //MOBILE TWO
+  $(".mobile-two .menu-toggle").click(function() {
+    $(this).toggleClass('open');
+    $(this).parent().next(".mobile-nav").toggle(0, "display");
+  });
+
+});
