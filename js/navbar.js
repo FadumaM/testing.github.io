15dcb09ca9bdc3f7441775d442196337dba044be
FadumaM <faduma.mohammed@gmail.com>
2017-09-11 18:43:41 +0100

    init new react project

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  //MOBILE TWO
-  $(".mobile-two .menu-toggle").click(function() {
-    $(this).toggleClass('open');
-    $(this).parent().next(".mobile-nav").toggle(0, "display");
-  });
-});
